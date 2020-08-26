ba6a5e64d14dc420d605b31e84c0e10afb2c2981
sktt1-win <gkswh4860@naver.com>
2020-08-26 11:24:20 +0900

    리퀘스트 실습

--- 리퀘스트.py
+++ 리퀘스트.py
@@ -1,4 +1,9 @@
 import requests as rs
-res = rs.get("https://www.naver.com/")
+res = rs.get("https://www.google.com/")
 res.raise_for_status() #res.status_code가 정상이 아니면 에러냄
 print("응답코드 : ", res.status_code) #200이면 정상
+print(len(res.text))
+#print(res.text)
+
+#with open("googleText.html", "w", encoding="utf8") as f:
+#    f.write(res.text) #googleText.html 파일을 만든 후 f라고 이름 붙이고 ()안에 정보를 적는다
