cd9b534f38cd629c0c96200b15f1fd3e82f7b7e5
sktt1-win <gkswh4860@naver.com>
2020-08-29 15:19:31 +0900

    bs4활용-1 끝

--- BeautifulSoup4/네이버웹툰/전자오락수호대.py
+++ BeautifulSoup4/네이버웹툰/전자오락수호대.py
@@ -9,12 +9,24 @@ soup = BeautifulSoup(res.text, "lxml")
 
 # 화면에 있는 전자오락수호대 제목과 링크 가져오가
 제목들 = soup.find_all("td", attrs = {"class" : "title"}) # find_all을 사용해서 조건에 맞는 태그가 전부 다 배열형태로 나옴
+#print(제목들)
 # print(제목들[1].a.get_text(),)
 # link = 제목들[1].a["href"]
 # print("https://comic.naver.com" + link)
 
-for 웹툰 in 제목들: 
-    제목 = 웹툰.a.get_text()
-    link = "https://comic.naver.com" + 웹툰.a["href"]
-    print(제목, link)
-#print(제목들)
+#웹툰 제목, 링크 가져오기
+# for 웹툰 in 제목들: 
+#     제목 = 웹툰.a.get_text()
+#     link = "https://comic.naver.com" + 웹툰.a["href"]
+#     print(제목, link)
+
+#평균 별점구하기
+평점 = soup.find_all("div", attrs = {"class" : "rating_type"})
+평점총합 = 0
+for 웹툰 in 평점:
+    #별점 = 웹툰.strong.get_text()
+    별점 = 웹툰.find("strong").get_text()
+    print(별점)
+    평점총합 += float(별점)
+print(평점총합)
+print(평점총합 / len(평점))
