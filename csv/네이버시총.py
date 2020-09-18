497883c6462503934bc37e5b52f5176185c1d387
sktt1-win <gkswh4860@naver.com>
2020-09-18 21:09:13 +0900

    csv file produce

--- csv/네이버시총.py
+++ csv/네이버시총.py
@@ -4,7 +4,17 @@ import requests
 
 url = 'https://finance.naver.com/sise/sise_market_sum.nhn?sosok=0&page=' # 맨뒤에 원하는 페이지를 붙여서 가지고오기
 
-for page in range(1, 2):
+#
+filename = "시가총액-200등.csv"
+f = open(filename, "w", encoding="utf-8-sig", newline="")
+writer = csv.writer(f)
+
+# 헤더 넣기
+title = 'N	종목명	현재가	전일비	등락률	액면가	시가총액	상장주식수	외국인비율	거래량	PER	ROE'.split("\t")
+# tap으로 구분되어 있어서 split을 하면 ["N", "종목명"....]형식으로 된다
+writer.writerow(title) # writerow : 행에 정보를 쓰는것인듯
+
+for page in range(1, 5):
     res = requests.get(url + str(page))
     res.raise_for_status()
     soup = BeautifulSoup(res.text, "html5lib")
@@ -15,4 +25,4 @@ for page in range(1, 2):
         if(len(colums) <= 1):
             continue # 의미없는 데이터는 스킵
         data = [colum.get_text().strip() for colum in colums] # td안의 text를 가져옴
-        print(data)
+        writer.writerow(data) # csv file produce
