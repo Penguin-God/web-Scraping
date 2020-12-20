231571b38b014896f0ed385df1bf54527160d187
Penguin-God <gkswh4860@naver.com>
2020-12-20 16:22:02 +0900

    pull commit

--- csv/네이버시총.py
+++ csv/네이버시총.py
@@ -5,8 +5,8 @@ import os
 
 url = 'https://finance.naver.com/sise/sise_market_sum.nhn?sosok=0&page=' # 맨뒤에 원하는 페이지를 붙여서 가지고오기
 
-script_dir = 'C:/Users/parkj/Desktop/프로그래밍/python/web-Scraping/csv'
 filename = "시가총액-200등.csv"
+script_dir = 'C:/Users/parkj/Desktop/프로그래밍/python/web-Scraping/csv'
 abs_file_path = os.path.join(script_dir, filename)
 f = open(abs_file_path, "w", encoding="utf-8-sig", newline="")
 writer = csv.writer(f)
